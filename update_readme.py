5b0da73a38c3e947959633f386c28026e4cf13a8
Jimmy Z. Hu <ZIJING_HU@Foxmail.com>
2021-01-17 18:53:54 -0500

    v2.2

--- update_readme.py
+++ update_readme.py
@@ -12,8 +12,8 @@ iso_time = repo_info['updated_at'].replace('Z', '+00:00')
 delta = int(datetime.now().timestamp())
 
 v_msg = f'# of videos:\t{int((len(os.listdir("cyberstar_dm"))-1)/2)}'
-storage_msg = f'Repo Size:\t\t{str(round(repo_info["size"]/1024, 2))} MB'
-update_msg = f'Timestamp:\t\t{str(delta)}'
+storage_msg = f'Repo Size:\t{str(round(repo_info["size"]/1024, 2))} MB'
+update_msg = f'Timestamp:\t{str(delta)}'
 
 with open('README.txt', 'w') as f:
     f.write('\n'.join([update_msg, storage_msg, v_msg]))
