e9718537567d969c164e48bb612fc66d8fd1fa1b
Jimmy Z. Hu <ZIJING_HU@Foxmail.com>
2021-01-17 20:55:26 -0500

    v2.2

--- update_readme.py
+++ update_readme.py
@@ -13,8 +13,8 @@ delta = int(datetime.now().timestamp())
 
 v_msg1 = f'# of videos (C):\t{int((len(os.listdir("cyberstar_dm"))-1)/2)}'
 v_msg2 = f'# of videos (R):\t{int((len(os.listdir("rookie_dm"))-1)/2)}'
-storage_msg = f'Repo Size:\t{str(round(repo_info["size"]/1024, 2))} MB'
-update_msg = f'Timestamp:\t{str(delta)}'
+storage_msg = f'Repo Size:\t\t{str(round(repo_info["size"]/1024, 2))} MB'
+update_msg = f'Timestamp:\t\t{str(delta)}'
 
 with open('README.txt', 'w') as f:
     f.write('\n'.join([update_msg, storage_msg, v_msg1, v_msg2]))
